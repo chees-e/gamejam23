faee3239b5b41a5e21de0bfc8dfc7c0d533f8900
unknown <uilykcir@gmail.com>
2023-02-04 01:41:18 -0800

    pulse added

--- util/const.py
+++ util/const.py
@@ -1,6 +1,11 @@
+import random
+
+import pygame
+
 size = width, height = 1500, 900
 
+root_counter_max = 29
 
-root_colour = "tan3" # TODO: Randomize?
-root_colour2 = "tan4"
+root_colour = (105, 65, 48)
+
 bg_colour = "white"
