055f937e006a1cc747f35b16599a6cab2cba3681
Nicholas <nichpang@student.ubc.ca>
2023-02-04 22:36:20 -0800

    added basic menu template

--- scenes/menu.py
+++ scenes/menu.py
@@ -1,35 +1,42 @@
 import pygame
-import util.const as G
 import pygame_menu
+
+from scenes import game, settings, credits_page
+
 
 # RV:
 # next scene, next scenes parameters
 # Entry point
 def run(screen, params):
+
     while True:
         menu = pygame_menu.Menu('', 1500, 900)
 
         menu.add.image('./assets/onlyclowns_transparent.png', scale=(0.5, 0.5))
-        menu.add.button('Play', start)
-        menu.add.button('Settings', settings)
-        menu.add.button('Credits', credits)
+        menu.add.button('Play', start, screen)
+        menu.add.button('Settings', go_to_settings, screen)
+        menu.add.button('Credits', go_to_credits, screen)
         menu.add.button('Quit', pygame_menu.events.EXIT)  # letsfuckinggoooo
 
         menu.mainloop(surface)
+
 
 # init pygame
 pygame.init()
 surface = pygame.display.set_mode((1500, 900))
 
-#define buttons
-def start():
-    # idk
-    return 1, {}
 
-def settings():
-    #open the settings menu
+# define buttons
+def start(screen):
+    game.run(screen, {})
+    return
+
+
+def go_to_settings(screen):
+    settings.run(screen, {})
     pass
 
-def credits():
-    #open the credits page
+
+def go_to_credits(screen):
+    credits_page.run(screen, {})
     pass
