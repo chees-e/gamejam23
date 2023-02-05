b86bf2a54046b4f3b610ab6b4e2a1d2ed11fd5f8
Nicholas <nichpang@student.ubc.ca>
2023-02-04 22:35:32 -0800

    added basic menu template

---
+++ scenes/settings.py
@@ -0,0 +1,33 @@
+import pygame
+import pygame_menu
+
+from scenes import menu
+
+
+# RV:
+# next scene, next scenes parameters
+# Entry point
+def run(screen, params):
+    while True:
+        settings = pygame_menu.Menu('', 1500, 900)
+
+        settings.add.button('Volume', volume, screen)
+        settings.add.button('Back', back, screen)
+
+        settings.mainloop(surface)
+
+
+# init pygame
+pygame.init()
+surface = pygame.display.set_mode((1500, 900))
+
+
+# define buttons
+def volume(screen):
+    print("open volume settings")
+    return
+
+
+def back(screen):
+    menu.run(screen, {})
+    return
