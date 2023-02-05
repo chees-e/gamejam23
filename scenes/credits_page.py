9d93aabfaac167f0f513c1c86b1c316b89cef5c6
Nicholas <nichpang@student.ubc.ca>
2023-02-05 13:20:22 -0800

    added menu_bg

--- scenes/credits_page.py
+++ scenes/credits_page.py
@@ -9,7 +9,7 @@ from scenes import menu
 # Entry point
 def run(screen, params):
     while True:
-        credits = pygame_menu.Menu('', 1500, 900, theme=mainmenu)
+        credits = pygame_menu.Menu('', 1500, 900, theme=credits_bg)
 
         credits.add.image('./assets/onlyclowns.png', scale=(0.5,0.5))
         credits.add.label('programming - ricky , shawn , nick')
@@ -23,15 +23,18 @@ def run(screen, params):
 # init pygame
 pygame.init()
 surface = pygame.display.set_mode((1500, 900))
-
-mainmenu = pygame_menu.themes.THEME_DEFAULT.copy()
-mainmenu.background_color = (4,4,4)
-mainmenu.widget_font = pygame_menu.font.FONT_MUNRO
-mainmenu.title_bar_style = pygame_menu.widgets.MENUBAR_STYLE_NONE
-mainmenu.widget_margin = (0, 10)
-mainmenu.widget_padding = 10
-mainmenu.widget_font_color = (255, 255, 255)
-mainmenu.widget_selection_effect = pygame_menu.widgets.RightArrowSelection(arrow_size=(30, 40))
+credits_bg = pygame_menu.themes.THEME_DEFAULT.copy()
+credits_bg.background_color = pygame_menu.baseimage.BaseImage(
+    image_path="./assets/menu_bg.png",
+    drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
+    drawing_offset=(0, 0)
+)
+credits_bg.widget_font = pygame_menu.font.FONT_MUNRO
+credits_bg.title_bar_style = pygame_menu.widgets.MENUBAR_STYLE_NONE
+credits_bg.widget_margin = (0, 10)
+credits_bg.widget_padding = 10
+credits_bg.widget_font_color = (255, 255, 255)
+credits_bg.widget_selection_effect = pygame_menu.widgets.RightArrowSelection(arrow_size=(30, 40))
 
 def back(screen):
     menu.run(screen, {})
