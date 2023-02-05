bd2700e7dbd20b9ee754779085061ba44c1a141a
Nicholas <nichpang@student.ubc.ca>
2023-02-05 13:15:07 -0800

    typo

--- scenes/menu.py
+++ scenes/menu.py
@@ -8,9 +8,8 @@ from scenes import game, settings, credits_page
 # next scene, next scenes parameters
 # Entry point
 def run(screen, params):
-
     while True:
-        menu = pygame_menu.Menu('', 1500, 900, theme=mainmenu)
+        menu = pygame_menu.Menu('', 1500, 900, theme=menu_bg)
 
         # menu.add.image('./assets/onlyclowns_transparent.png', scale=(0.5, 0.5))
         menu.add.button('Play', start, screen, font_size=50)
@@ -27,20 +26,20 @@ pygame.init()
 surface = pygame.display.set_mode((1500, 900))
 
 # create custom theme
-mainmenu = pygame_menu.themes.THEME_DEFAULT.copy() #copy default theme
+menu_bg = pygame_menu.themes.THEME_DEFAULT.copy()  # copy default theme
 mmbg = pygame_menu.baseimage.BaseImage(
     image_path="./assets/title-screen.png",
     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
-    drawing_offset=(0,0)
+    drawing_offset=(0, 0)
 
 )
-mainmenu.background_color = mmbg #set theme background color as the image
-mainmenu.widget_font = pygame_menu.font.FONT_8BIT
-mainmenu.title_bar_style = pygame_menu.widgets.MENUBAR_STYLE_NONE
-mainmenu.widget_margin = (0, 30)
-mainmenu.widget_padding = 10
-mainmenu.widget_font_color = (255, 255, 255)
-mainmenu.widget_selection_effect = pygame_menu.widgets.RightArrowSelection(arrow_size=(30, 40))
+menu_bg.background_color = mmbg  # set theme background color as the image
+menu_bg.widget_font = pygame_menu.font.FONT_8BIT
+menu_bg.title_bar_style = pygame_menu.widgets.MENUBAR_STYLE_NONE
+menu_bg.widget_margin = (0, 30)
+menu_bg.widget_padding = 10
+menu_bg.widget_font_color = (255, 255, 255)
+menu_bg.widget_selection_effect = pygame_menu.widgets.RightArrowSelection(arrow_size=(30, 40))
 
 
 # define buttons
@@ -48,9 +47,11 @@ def start(screen):
     game.run(screen, {})
     return
 
+
 def go_to_tutorial(screen):
-    #tutorial page
+    # tutorial page
     pass
+
 
 def go_to_settings(screen):
     settings.run(screen, {})
